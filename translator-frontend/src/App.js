4d020c2b0511f56f80d05a5a8e5b1b168d4da44e
Mahesh Rajannan <mahesh.rajannan@gmail.com>
2020-06-08 13:00:34 -0500

    reduced instances
    reduced number of instances for troubleshooting

--- translator-frontend/src/App.js
+++ translator-frontend/src/App.js
@@ -21,8 +21,8 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        console.log("Contacting localhost:8080");
-        fetch('http://localhost:8080/sentiment', {
+        console.log("Contacting 192.168.99.104:30465");
+        fetch('http://192.168.99.104:30465/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
