687c139bc94257a733adb12e8884da54491f0c33
Mahesh Rajannan <mahesh.rajannan@gmail.com>
2020-07-18 22:21:00 -0500

    working with minikube
    working with minikube

--- StopKubernetesDeployments.sh
+++ StopKubernetesDeployments.sh
@@ -1,19 +1,21 @@
-echo '1/5: Rest Docker to prevent connection error'
+echo '1/4: Rest Docker to prevent connection error'
 unset DOCKER_HOST
 unset DOCKER_TLS_VERIFY
 unset DOCKER_TLS_PATH
 
-echo '2/5: Deleting Deployments'
+echo '2/4: Deleting Deployments'
 kubectl get deployments
 kubectl delete deployment translator-frontend
 kubectl delete deployment sa-logic
 kubectl delete deployment sa-web-app
 kubectl get deployments
 
-echo '3/5: Deleting Services'
+echo '3/4: Deleting Services'
 kubectl delete service translator-frontend-lb
 kubectl delete service sa-logic
 kubectl delete service sa-web-app-lb
 minikube service list
 
+echo '3/4: Stopping Minikube'
 minikube stop
+minikube status
--- translator-frontend/src/App.js
+++ translator-frontend/src/App.js
@@ -21,8 +21,8 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        console.log("Contacting 192.168.99.110:30976");
-        fetch('http://192.168.99.110:30976/sentiment', {
+        console.log("Contacting 192.168.99.110:30454");
+        fetch('http://192.168.99.110:30454/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
