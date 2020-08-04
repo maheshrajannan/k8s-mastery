b65bdc3f2ed2e525666b41c5279dcdbbbb7b294b
Mahesh Rajannan <mahesh.rajannan@gmail.com>
2020-08-03 22:05:46 -0500

    simple script rename.
    simple script rename.

--- RunTranslatorMinikube.sh
+++ RunTranslatorMinikube.sh
@@ -6,7 +6,7 @@
 # minikube --vm-driver=virtualbox start
 # minikube stop
 # My mac has 8 core 
-# minikube start --memory 10240 --cpus=4 --vm-driver=virtualbox
+minikube start --memory 10240 --cpus=4 --vm-driver=virtualbox
 echo '1/20: Is Minikube running ?'
 minikube status
 echo '2/20: Reset Docker to prevent connection error'
