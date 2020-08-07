01cf980ec8535acae21d784bb6b027d0c809685e
Mahesh Rajannan <mahesh.rajannan@gmail.com>
2020-08-06 22:07:56 -0500

    Running in gCloud
    Running in gCloud

--- HowToRun/GKE/deleteGCloudCluster.sh
+++ HowToRun/GKE/deleteGCloudCluster.sh
@@ -1,4 +1,8 @@
 set -e
 # TODO: Check gCloud has the cluster. If present then delete.
+# Maheshs-MBP-3:GKE maheshrajannan$ kubectl config get-contexts -o name | grep translator
+# gke_translator-258700_us-central1-f_translator3
+# Maheshs-MBP-3:GKE maheshrajannan$ kubectl config delete-cluster gke_translator-258700_us-central1-f_translator3
+# deleted cluster gke_translator-258700_us-central1-f_translator3 from /Users/maheshrajannan/.kube/config
 yes | gcloud container clusters delete translator3
 echo "Successfully deleted gCloud Cluster"
