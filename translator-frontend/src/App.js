df862e09ed01498686078397eceae6e6a7985d3a
Mahesh Rajannan <mahesh.rajannan@gmail.com>
2020-08-29 23:03:28 -0500

    npm audit fix
    npm audit fix

--- translator-frontend/src/App.js
+++ translator-frontend/src/App.js
@@ -21,8 +21,8 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        console.log("Contacting localhost:8080");
-        fetch('http://localhost:8080/sentiment', {
+        console.log("Contacting 34.122.2.155:80");
+        fetch('http://34.122.2.155:80/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
